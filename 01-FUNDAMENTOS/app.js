eff99b43fda193db144bf759aa16084fd8646459
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-04-13 18:06:52 -0500

    Introduccion a la consola

--- 01-FUNDAMENTOS/app.js
+++ 01-FUNDAMENTOS/app.js
@@ -3,8 +3,23 @@
 
 let a = 10,
     b = 20,
-    c = 10,
-    d = 10,
+    c = 'Hola ',
+    d = 'Spiderman',
     x = a + b;
 
-    console.log(x);
+   console.log("a",a);
+   console.log("b",b);
+   console.log("c",c);
+
+   // Manera simplificada para mostra la variable y su valor
+   console.log('%c Mis Variables', 'color:red; font-weight:bold');
+   console.log({a});
+   console.log({b});
+   console.log({c});
+
+   // Mostrar variables mediante tablas
+   console.log('%c Mis Variables en Tablas', 'color:green; font-weight:bold');
+   console.table({a,b,c,d,x});
+
+   // Concatenación de Variables
+   const saludo = c + d;
