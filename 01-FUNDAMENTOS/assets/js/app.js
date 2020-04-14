f8bed70da660886079cc49688e8bb0975bbaaf52
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-04-14 00:26:35 -0500

    Orden y lugar de las importaciones

---
+++ 01-FUNDAMENTOS/assets/js/app.js
@@ -0,0 +1,39 @@
+alert("Hola mundo desde app.js");
+
+// console.log(console.log('Hola Mundo'));
+
+
+// Otra manera de declarar variables
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+   // console.log("a",a);
+   // console.log("c",c); 
+   // console.log("b",b);
+
+   // Manera simplificada para mostra la variable y su valor
+   // console.log('%c Mis Variables', 'color:red; font-weight:bold');
+   // console.log({a});
+   // console.log({b});
+   // console.log({c});
+
+   // Concatenación de Variables
+   const saludo = c + d;
+
+   // console.log("-------------------------------------");
+   // Mostrar variables mediante tablas
+   // console.log('%c Mis Variables en Tablas', 'color:green; font-weight:bold');
+
+   c = 'Hola de nuevo';
+   
+   // console.table({a,b,c,d,x});
+
+
+   var miNombre = 'Marco';
+
+
+
