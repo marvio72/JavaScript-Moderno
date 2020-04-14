cee4381c8735e75f62a114009d53ccf44d1fe017
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-04-14 15:35:45 -0500

    Palabras reservadas y nombres de variables

---
+++ 01-FUNDAMENTOS/assets/js/palabras-reservadas.js
@@ -0,0 +1,25 @@
+
+// let const = 123; Mal palabra reservada
+let constante = 123;
+
+
+// Permitidos
+let objeto$ = 123;
+let precio_99_99 = 123;
+
+let jugadorConPuntajeMasAlto = 'Marco';
+ class JuegoAnio {
+
+ }
+
+
+ // No permitidos
+
+ let 1objeto = 123;
+ let precio99.99 = 123;
+
+ let jugador con puntaje mas alto = "marco;"
+
+ class JuegoAño {
+   
+ }
