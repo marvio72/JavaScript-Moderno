c2f51ebd46688fc2f7fa54f919c77286775ce626
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-04-21 23:38:09 -0500

    Alternativa al if Else

--- 01-FUNDAMENTOS/assets/js/if-else.js
+++ 01-FUNDAMENTOS/assets/js/if-else.js
@@ -26,3 +26,16 @@ if (dia === 0) {
   console.log('Ningun dia Seleccionado');
 }
 
+
+//Sin usar If Else, o Switch, únicamente objetos
+dia = 13;
+diaLetra=['domingo','lunes','martes','miércoles','jueves','viernes','sábado',];
+const diaLetras = {
+  0: 'domingo',
+  1: 'lunes',
+  2: 'martes',
+  3: 'miércoles'
+};
+// dia de la semana
+console.log(diaLetra[dia] || 'Día no definido');
+console.log(diaLetras[dia] || 'Día no definido');
