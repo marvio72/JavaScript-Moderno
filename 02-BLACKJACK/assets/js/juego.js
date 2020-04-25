2b6f085262c9e09251823bb51aca85c80ea12481
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-04-24 19:12:41 -0500

    Valor de Cada Carta

--- 02-BLACKJACK/assets/js/juego.js
+++ 02-BLACKJACK/assets/js/juego.js
@@ -13,9 +13,7 @@ const especiales = ['A','J','K','Q'];
 /*==============================================================================================
 FUNCIONES
 ==============================================================================================*/
-/*
-Esta función crea una nueva varaja
-*/
+// Esta función crea una nueva varaja
 const crearDeck = () => {
   for (let i = 2; i <=10; i++) {
     for (const tipo of tipos) {
@@ -32,23 +30,39 @@ const crearDeck = () => {
   return deck;
 };
 
-/*
-Esta función me permite tomar una carta.
-*/
-
+//Esta función me permite tomar una carta.
 const pedirCarta = () => {
 
+  crearDeck();
+
   if (deck.length === 0) {
-    throw("No hay mas cartas");
+    throw "No hay mas cartas";
   }
   
   const carta = deck.pop();
+
+  console.log(deck);
+  console.log(carta);
   return carta;
 };
 
-console.log(crearDeck());
+// Esta función le da un valor a cada carta
+const valorCarta = (carta) => {
+  const valor = carta.substring(0,carta.length - 1);
+  // console.log(valor);
 
-for (let i = 0; i <= 100; i++) {
-  pedirCarta();
-  console.log(deck);
-}
+  // if (isNaN(valor)) {
+  //   console.log('No es un número');
+  //   puntos = (valor === 'A') ? 11 : 10;
+  // }else{
+  //   console.log('Es un número');
+  //   puntos = parseInt(valor);
+  // }
+
+  return (isNaN(valor)) ? 
+         (valor === 'A') ? 11 : 10 : 
+         (valor * 1);
+};
+
+
+console.log({valor: valorCarta(pedirCarta())});
