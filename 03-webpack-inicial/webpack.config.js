5de06000ad15018e7190d43a7c79f8a5a744a8cc
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-05-07 06:50:36 -0500

    Archivo de Configuracion del webPack

---
+++ 03-webpack-inicial/webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin'); 
+
+module.exports = {
+
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    attributes: false,
+                    minimize: false
+                },
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+    ]
+
+}
+
