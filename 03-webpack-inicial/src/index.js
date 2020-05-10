9c890ea4195d75c5cf7d9bcab7f28a63a55c62af
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-05-10 01:46:40 -0500

    Agregar carpeta SASS y mover archivo estilos.scss

--- 03-webpack-inicial/src/index.js
+++ 03-webpack-inicial/src/index.js
@@ -2,9 +2,9 @@ import {
   saludar
 } from './js/componentes.js';
 // import './styles.css';
-import './estilos.scss';
+import './sass/estilos.scss';
 // import img from './assets/dcd5e077cf9f54ebe52d4f7ebe8c3080.png';
 
-const nombre = 'Marco';
+const nombre = 'Marco 48';
 
 saludar(nombre);
